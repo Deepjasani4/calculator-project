15490132750c2f716c4cf354e0d9c858e2d84601
Deep jasani <143923173+Deepjasani4@users.noreply.github.com>
2026-04-14 15:02:23 +0530

    Delete js-practies directory

--- js-practies/AreaReactangle.js
+++
@@ -1,6 +0,0 @@
-function calculateArea(length, width){
-    const area = length * width;
-    console.log("Area of rectangle is..." +area);
-}
-
-calculateArea(10,5);
--- js-practies/MultiplicationTable.js
+++
@@ -1,6 +0,0 @@
-function multiplicationTable (firstNum, LastNum){
-    for(let i = 1; i <= LastNum; i++){
-        console.log(`${firstNum} * ${i} = ${firstNum * i}`);
-    }
-}
-multiplicationTable(5,10);
